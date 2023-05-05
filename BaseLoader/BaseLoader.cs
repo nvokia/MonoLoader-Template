e6fa7586769cf893699478ff54fd8e168e064163
SimAFK <130346503+SimAFK@users.noreply.github.com>
2023-05-05 01:11:51 -0700

    Add project files.

---
+++ BaseLoader/BaseLoader.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using MainCheat;
+
+namespace BaseLoader
+{
+    public class Loader
+    {
+        public static void Init()
+        {
+            // Loads our main class through game object
+            Loader.Load = new GameObject();
+
+            // Adds our main class to the game object
+            Loader.Load.AddComponent<Main>();
+
+            // Prevents the game object from being destroyed on scene change
+            UnityEngine.Object.DontDestroyOnLoad(Loader.Load);
+        }
+
+        public static void unload()
+        {
+            _unload();
+        }
+
+        private static void _unload()
+        {
+            GameObject.Destroy(Load);
+        }
+
+        private static GameObject Load;
+    }
+}
---
+++ BaseLoader/Main.cs
@@ -0,0 +1,30 @@
+﻿using BaseLoader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace MainCheat
+{
+    class Main : MonoBehaviour
+    {
+        public void Start()
+        {
+            GUI.Label(new Rect(0f, 0f, 100f, 30f), "Base");
+        }
+        public void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.End))
+            {
+                Loader.unload();
+            }
+        }
+        public void OnGUI()
+        {
+
+        }
+    }
+}
